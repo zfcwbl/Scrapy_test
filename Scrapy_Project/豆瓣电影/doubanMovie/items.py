350e5a9e9604fc4a4276bc1533a705b8efd82378
zfc <1406468717@qq.com>
2019-09-10 10:31:53 +0800

    添加注释

--- Scrapy_Project/豆瓣电影/doubanMovie/items.py
+++ Scrapy_Project/豆瓣电影/doubanMovie/items.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 class DoubanmovieItem(scrapy.Item):
+    #定义需要获取的信息
     rank = scrapy.Field()
     title = scrapy.Field()
     link = scrapy.Field()
--- Scrapy_Project/豆瓣电影/doubanMovie/main.py
+++ Scrapy_Project/豆瓣电影/doubanMovie/main.py
@@ -1,6 +1,7 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
+'''通过这个文件启动爬虫，可以进行调试'''
 if __name__=='__main__':
     process=CrawlerProcess(get_project_settings())
     process.crawl('doubanmovie')
